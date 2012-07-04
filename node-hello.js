71f00874aed128648c7716940e1f05e6ae68bd9a
willthai <will@tofubox.(none)>
2012-07-04 01:21:00 -0700

    added hbs, handlebars wrapper for express 3.x support

--- node-hello.js
+++ node-hello.js
@@ -1,11 +1,11 @@
 var express = require('express'),
 	app = express.createServer(),
-    Y = require('yui').use('oop', 'base-base'),
+    Y = require('yui3').use('oop', 'base-base'),
 	port = 8080,
-	path = __dirname + '/static',
-	DataProvider = require('./modules/db.js').DataProvider;
+	path = __dirname + '/static';
+//	DataProvider = require('./modules/db.js').DataProvider;
 
-var dataProvider = new DataProvider('127.0.0.1', 27017)
+//var dataProvider = new DataProvider('127.0.0.1', 27017)
 
 console.log('path is' + path)
 console.log('listening on port:'+port);
@@ -14,12 +14,15 @@ app.use(app.router);
 app.use(express.directory(path))
 app.use(express.static(path))
 
-app.set('view engine', 'handlebars')
-app.set('view options', { layout: false})
-app.register('.html', require('handlebars'))
-
+//app.register('.html', require('handlebars'))
+app.configure(function() {
+    app.set('view engine', 'html')
+    app.set("view options", {layout: false});
+    app.set('views', __dirname + '/views');
+    app.engine('html', require('hbs').__express);
+})
 app.get('/', function(req, res) {
-	var data = {
+/*	var data = {
 			header: 'Pinching pennies',
 			content: 'efficient.'
 		},
@@ -31,14 +34,17 @@ app.get('/', function(req, res) {
 		console.log(data)
 		res.render('index.html', data);
 	});
+*/
+	res.render('index.html');
 })
 app.get('/insertProduct', function(req,res) {
 	console.log('ajax')
 	console.log(req.query)
-	dataProvider.insert(req.query, function(err,data) {
+/*	dataProvider.insert(req.query, function(err,data) {
 		console.log('inserted');
 		console.log(data)
 	});
+*/
 })
 app.get('/getProduct', function(req,res) {
 	var q = req.query,
@@ -49,12 +55,13 @@ app.get('/getProduct', function(req,res) {
 				console.log(err)
 			}
 		};
-	dataProvider.getProduct(q, returnProduct);
+//	dataProvider.getProduct(q, returnProduct);
 });
 app.get('/getAll', function(req, res) {
-	dataProvider.getAll(function(error, data) {
+/*	dataProvider.getAll(function(error, data) {
 		console.log('getAll')
 		console.log(data)
 	});
+*/
 });
 app.listen(port)
